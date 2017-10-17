6cc45cb5f7584d1308f61a651610a0fcfbd870c5
Liuml <951442843@qq.com>
2017-10-17 15:07:45 +0800

    checkbox 问题解决

--- js/my/CustomKeyPage.js
+++ js/my/CustomKeyPage.js
@@ -1,128 +1,5 @@
-/*
- /!**
- * Created by liuml on 2017/9/17.
- *!/
- import React, {Component} from 'react';
- import {
- AppRegistry,
- StyleSheet,
- Text,
- View,
- Image,
- TouchableOpacity
- } from 'react-native';
- /!**
- * 自定义分类页面
- *!/
- import NavigationBar from '../compoent/NavigationBar';
- import CheckBox from 'react-native-check-box';
- import ArrayUtil from '../util/ArrayUtil';
-
- export default class CustomKeyPage extends Component {
-
- // 构造
- constructor(props) {
- super(props);
- // 初始状态
- this.state = {
- data: [
- {name: 'Android', checked: true},
- {name: 'IOS', checked: false},
- {name: 'React Native', checked: true},
- {name: 'Java', checked: false},
- {name: 'JS', checked: true}
- ]
- };
- }
-
- handleBack = () => {
- this.props.navigation.goBack();
- }
- getLeftBtn = () => {
- return <View style={{flexDirection: 'row', alignItems: 'center'}}>
- <TouchableOpacity
- activeOpacity={0.7}
- onPress={this.handleBack}>
- <Image source={require('../../res/images/ic_arrow_back_white_36pt.png')}
- style={{width: 24, height: 24}}/>
- </TouchableOpacity>
- </View>;
- }
-
- getRightBtn = () => {
- return <View style={{flexDirection: 'row', alignItems: 'center'}}>
- <TouchableOpacity activeOpacity={0.7}>
- <View style={{marginRight: 10}}>
- <Text style={{fontsize: 16, color: '#FFF'}}>保存</Text>
- </View>
- </TouchableOpacity>
- </View>
- }
-
- //CheckBox 点击  有个疑问为什么在这里设置值就可以不用setState就改变item的checked,因为是这样调用的()=>this.handlerCBClick(item)
- /!* handleClick = (item) => {
- console.log(this.state.data);
- // console.log(item.checked);
- item.checked = !item.checked;
- }*!/
- handleClick = (item) => {
- console.log(this.state.data);
- item.checked = !item.checked;
- }
- //渲染CheckBox  这里item就是一个对象
- renderCheckBox = (item) => {
- // console.log(item.name);
- console.log(item.name + ',' + item.checked);
- return <CheckBox
- onClick={() => this.handleClick(item)}
- isChecked={item.checked}
- style={{flex: 1, padding: 10}}
- leftText={item.name}
- unCheckedImage={<Image source={require('../../res/images/ic_check_box_outline_blank.png')}
- style={styles.checkbox}/>}
- checkedImage={<Image source={require('../../res/images/ic_check_box.png')} style={styles.checkbox}/>}
- />
- }
-
- //渲染checkbox数组
- renderViews = () => {
- let len = this.state.data.length;
- var Views = [];//要绘制的所有多选框，装入views数组
- for (let i = 0; i < len; i++) {
- Views.push((
- <View key={`view_${i}`} style={{flexDirection: 'column'}}>
- {this.renderCheckBox(this.state.data[i])}
- </View>
- ));
- }
- return Views;
- }
-
- render() {
- return <View style={styles.container}>
- <NavigationBar
- title="自定义分类"
- rightButton={this.getRightBtn()}
- leftButton={this.getLeftBtn()}/>
- <View style={{flexDirection: 'column'}}>
- {this.renderViews()}
- </View>
- </View>
- }
- }
-
- const styles = StyleSheet.create({
-
- container: {
- flex: 1
- },
- checkbox: {
- tintColor: '#63B8FF'
- }
-
- });*/
 /**
- * Created by david on 10/4/2017.
+ * Created by liuml on 2017/9/17.
  */
 import React, {Component} from 'react';
 import {
@@ -131,42 +8,31 @@ import {
     Text,
     View,
     Image,
-    TouchableOpacity,
-    AsyncStorage
+    AsyncStorage,
+    TouchableOpacity
 } from 'react-native';
-
+/**
+ * 自定义分类页面
+ */
 import NavigationBar from '../compont/NavigationBar';
-import CheckBox from "react-native-check-box";
-// import CheckBox from "react-native-checkbox";
-
+import CheckBox from 'react-native-check-box';
+import Toast from "react-native-easy-toast"
 export default class CustomKeyPage extends Component {
 
+    // 构造
     constructor(props) {
         super(props);
+        // 初始状态
         this.state = {
-            data: [
-                {name: 'Android', checked: true},
-                {name: 'IOS', checked: false},
-                {name: 'React Native', checked: true},
-                {name: 'Java', checked: true},
-                {name: 'JS', checked: true}
-            ]
+            data: [{name: 'android', checked: true},
+                {name: 'IOS', checked: false}]
         };
     }
 
     handleBack = () => {
-        //把任务栈顶部的任务清除
         this.props.navigation.goBack();
     }
-
-    /* //保存
-     handleSave = ()=>{
-     //AsyncStorage是一个简单的、异步的、持久化的Key-Value存储系统
-     AsyncStorage.setItem('custom_key',JSON.stringify(this.state.data))
-     .then(()=> this.refs.toast.show("保存成功"));
-     }
-     */
-    getNavLeftBtn = () => {
+    getLeftBtn = () => {
         return <View style={{flexDirection: 'row', alignItems: 'center'}}>
             <TouchableOpacity
                 activeOpacity={0.7}
@@ -177,11 +43,12 @@ export default class CustomKeyPage extends Component {
         </View>;
     }
 
-    getNavRightBtn = () => {
+
+    getRightBtn = () => {
         return <View style={{flexDirection: 'row', alignItems: 'center'}}>
             <TouchableOpacity
-                activeOpacity={0.7}
-            >
+                onPress={this.handleSave}
+                activeOpacity={0.7}>
                 <View style={{marginRight: 10}}>
                     <Text style={{fontSize: 16, color: '#FFF'}}>保存</Text>
                 </View>
@@ -189,16 +56,27 @@ export default class CustomKeyPage extends Component {
         </View>;
     }
 
-
-    handleClick = (item) => {
-       // item.checked = !item.checked;
-        console.log(`item = ${item}`);
-        // item.checked = !item.checked;
+    //保存
+    handleSave = () => {
+        //http://lib.csdn.net/article/reactnative/43540   JSON.stringify 字符串转JSON
+        //AsyncStorage是一个简单的、异步的、持久化的Key-Value存储系统
+        AsyncStorage.setItem('custom_key', JSON.stringify(this.state.data))
+            .then(() => this.refs.toast.show("保存成功"));
+        console.log(JSON.stringify(this.state.data));
     }
 
+    //CheckBox 点击  有个疑问为什么在这里设置值就可以不用setState就改变item的checked,因为是这样调用的()=>this.handlerCBClick(item)
+    handleClick = (item) => {
+        console.log("之前 " + item.checked);
+        item.checked = !item.checked;
+        console.log("之后 " + item.checked);
+    }
+    //渲染CheckBox  这里item就是一个对象
     renderCheckBox = (item) => {
+        console.log(item);
         console.log(item.name + ',' + item.checked);
-        return (<CheckBox
+        var leftText = item.name;
+        return <CheckBox
             style={{flex: 1, padding: 10}}
             onClick={() => this.handleClick(item)}
             leftText={item.name}
@@ -206,22 +84,8 @@ export default class CustomKeyPage extends Component {
             unCheckedImage={<Image source={require('../../res/images/ic_check_box_outline_blank.png')}
                                    style={styles.checkbox}/>}
             checkedImage={<Image source={require('../../res/images/ic_check_box.png')} style={styles.checkbox}/>}
-        />)
+        />
     }
-    // renderCheckBox = (item) => {
-    //     console.log(item.name + ',' + item.checked);
-    //     return (<CheckBox
-    //         style={{flex: 1, padding: 10}}
-    //         label='Label'
-    //         onChange={(checked) => console.log('I am checked', checked)}
-    //         onClick={() => this.handleClick(item)}
-    //         leftText={item.name}
-    //         isChecked={item.checked}
-    //         unCheckedImage={<Image source={require('../../res/images/ic_check_box_outline_blank.png')}
-    //                                style={styles.checkbox}/>}
-    //         checkedImage={<Image source={require('../../res/images/ic_check_box.png')} style={styles.checkbox}/>}
-    //     />)
-    // }
 
     renderViews = () => {
         let len = this.state.data.length;
@@ -248,38 +112,57 @@ export default class CustomKeyPage extends Component {
 
         return views;
     }
+    componentDidMount = () => {
+
+
+        this.loadData();
+
+    }
+
+    loadData = () => {
+        //加载本地数据
+        AsyncStorage.getItem('custom_key')
+            .then(value => {
+                //有用户数据，选中该选中CheckBox
+                if (value !== null) {
+                    console.log(JSON.parse(value));
+                    this.setState({data: JSON.parse(value)});
+                } else {
+                    this.setState({
+                        data: [{name: 'Android', checked: true},
+                            {name: 'IOS', checked: true},
+                            {name: 'React Native', checked: true},
+                            {name: 'Java', checked: true},
+                            {name: 'JS', checked: false}]
+                    });
+                }
+            });
+
+        // console.log(this.state.data);
+    }
 
     render() {
         return <View style={styles.container}>
             <NavigationBar
                 title="自定义分类"
-                rightButton={this.getNavRightBtn()}
-                leftButton={this.getNavLeftBtn()}/>
+                rightButton={this.getRightBtn()}
+                leftButton={this.getLeftBtn()}/>
             <View style={{flexDirection: 'column'}}>
                 {this.renderViews()}
             </View>
-            {/*<Toast ref="toast"/>*/}
-        </View>;
-    }
-
-    componentDidMount = () => {
-        //加载本地数据
-        /* AsyncStorage.getItem('custom_key')
-         .then(value=>{
-         //有用户数据，选中该选中CheckBox
-         if(value !== null){
-         this.setState({data:JSON.parse(value)});
-         }
-         });*/
-
+            <Toast ref="toast"/>
+        </View>
     }
 }
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1
     },
     checkbox: {
         tintColor: '#63B8FF'
     }
+
 });
+
